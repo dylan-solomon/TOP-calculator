55e4bd0c811fa3e9bcff572f5ddf909102ca6d92
Dylan Solomon <dylansolomon413@gmail.com>
2022-04-04 13:01:23 -0700

    Add output updating in Javascript

---
+++ script.js
@@ -0,0 +1,28 @@
+const output = document.querySelector('.result');
+
+const nums = {
+    1: document.getElementById('one'),
+    2: document.getElementById('two'),
+    3: document.getElementById('three'),
+    4: document.getElementById('four'),
+    5: document.getElementById('five'),
+    6: document.getElementById('six'),
+    7: document.getElementById('seven'),
+    8: document.getElementById('eight'),
+    9: document.getElementById('nine'),
+    0: document.getElementById('zero'),
+    'mulitply': document.querySelector('.multiply'),
+    'add': document.querySelector('.plus'),
+    'subtract': document.querySelector('.subtract'),
+    'divide': document.querySelector('.divide'),
+}
+
+/* max output length: 14 */
+
+output.textContent = 0;
+
+for (let num in nums) {
+    nums[num].addEventListener('click', () => {
+        output.textContent = nums[num].textContent;
+    });
+}
